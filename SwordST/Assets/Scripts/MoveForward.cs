9b47d6c86b1f47e2143d3d0c6fcc183c2095a321
FreddyJonas <filip.rosquist@elev.ga.lbs.se>
2020-02-04 15:27:53 +0100

    lol

---
+++ SwordST/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public GameObject myPlayer;
+
+    public void MoveForward()
+    {
+        myPlayer.transform.position += new Vector3(moveSpeed, 0, 0);
+    }
+   
+}
