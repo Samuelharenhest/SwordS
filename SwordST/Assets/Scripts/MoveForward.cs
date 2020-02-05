90bc5ac15bfd9fdf91e10a8ff404f9fae49ab48c
FreddyJonas <filip.rosquist@elev.ga.lbs.se>
2020-02-05 10:45:29 +0100

    lol

--- SwordST/Assets/Scripts/MoveForward.cs
+++ SwordST/Assets/Scripts/MoveForward.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 1f;
     public GameObject myPlayer;
+    Vector3 myTargetPosition;
 
-    public void MoveForward()
+    private void Start()
     {
-        myPlayer.transform.position += new Vector3(moveSpeed, 0, 0);
+        myTargetPosition = myPlayer.transform.position;
     }
-   
+
+    private void Update()
+    {
+        myPlayer.transform.position = Vector3.MoveTowards(myPlayer.transform.position, myTargetPosition, 0.1f);
+    }
+
+    public void Forward()
+    {
+        myTargetPosition = myPlayer.transform.position + new Vector3(moveSpeed, 0, 0);
+    }
+
+
 }
